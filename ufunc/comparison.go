b59eb74b29dff024897c7503168adc3a585e78e1
AkhilHector <akhilhector.1@gmail.com>
2017-10-12 21:37:09 -0500

    Added Equal() and Not_Equal() funcs in numgo/ufunc dir
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ ufunc/comparison.go
@@ -0,0 +1,57 @@
+/* This Source Code Form is subject to the terms of the MPL
+*  License. If a copy of the same was not distributed with this
+*  file, You can obtain one at
+*  https://github.com/AkhilHector/pubundsci/blob/master/LICENSE.
+ */
+
+package numgo
+
+import (
+	"log"
+)
+
+func extract_parameters(args []int) []int {
+	var parameters []int
+
+	// store the parameters in a slice
+	for _, p := range args {
+		parameters = append(parameters, p)
+	}
+	return parameters
+}
+
+func Equal(array_one, array_two []int) []bool {
+	var result []bool
+
+	// check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Equal() operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] == array_two[i] {
+				result = append(result, true)
+			} else {
+				result = append(result, false)
+			}
+		}
+	}
+	return result
+}
+
+func Not_Equal(array_one, array_two []int) []bool {
+	var result []bool
+
+	// check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Not_Equal operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] != array_two[i] {
+				result = append(result, true)
+			} else {
+				result = append(result, false)
+			}
+		}
+	}
+	return result
+}
