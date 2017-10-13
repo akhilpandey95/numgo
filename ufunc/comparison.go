f74f0c7f122eb2273af4f1f0d1f22c3ea2d75164
AkhilHector <akhilhector.1@gmail.com>
2017-10-13 14:15:57 -0500

    Added Greater(), Lesser() functions to ufunc/comparision , updated gitignore
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- ufunc/comparison.go
+++ ufunc/comparison.go
@@ -55,3 +55,39 @@ func Not_Equal(array_one, array_two []int) []bool {
 	}
 	return result
 }
+
+func Greater(array_one, array_two []int) []int {
+	var result []int
+
+	// check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Greater operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] >= array_two[i] {
+				result = append(result, array_one[i])
+			} else {
+				result = append(result, array_two[i])
+			}
+		}
+	}
+	return result
+}
+
+func Lesser(array_one, array_two []int) []int {
+	var result []int
+
+	//check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Lesser operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] <= array_two[i] {
+				result = append(result, array_one[i])
+			} else {
+				result = append(result, array_two[i])
+			}
+		}
+	}
+	return result
+}
