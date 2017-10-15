c35d71507b60899baea3d78e7b86eb1fd80810e9
AkhilHector <akhilhector.1@gmail.com>
2017-10-14 23:54:07 -0500

    Added Greater_B() and Lesser_B() functions that are a bool version of Greater(), Lesser() funcs
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- ufunc/comparison.go
+++ ufunc/comparison.go
@@ -74,6 +74,24 @@ func Greater(array_one, array_two []int) []int {
 	return result
 }
 
+func Greater_B(array_one, array_two []int) []bool {
+	var result []bool
+
+	// check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Greater operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] >= array_two[i] {
+				result = append(result, true)
+			} else {
+				result = append(result, false)
+			}
+		}
+	}
+	return result
+}
+
 func Lesser(array_one, array_two []int) []int {
 	var result []int
 
@@ -86,6 +104,24 @@ func Lesser(array_one, array_two []int) []int {
 				result = append(result, array_one[i])
 			} else {
 				result = append(result, array_two[i])
+			}
+		}
+	}
+	return result
+}
+
+func Lesser_B(array_one, array_two []int) []bool {
+	var result []bool
+
+	//check the args and return the appropriate function result
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: Shapes of two arrays must be similar to perform Lesser operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			if array_one[i] <= array_two[i] {
+				result = append(result, true)
+			} else {
+				result = append(result, false)
 			}
 		}
 	}
