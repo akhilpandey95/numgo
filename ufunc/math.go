bf3fc2b599f9f380a0fceed1a2455b70774ce4c7
AkhilHector <akhilhector.1@gmail.com>
2017-10-15 21:05:01 -0500

    Added math functions to ufunc sub module
    - ufunc.Sin() for Sin operation on every element of Array
    - ufunc.Cos() for Cos operation on every element of Array
    - ufunc.Tan() for Tan operation on every element of Array
    
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ ufunc/math.go
@@ -0,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the MPL
+*  License. If a copy of the same was not distributed with this
+*  file, You can obtain one at
+*  https://github.com/AkhilHector/pubundsci/blob/master/LICENSE.
+ */
+
+package numgo
+
+import (
+	"math"
+)
+
+func Sin(args ...int) []float64 {
+	var result []float64
+	parameters := extract_parameters(args)
+
+	// check the args and return the appropriate function result
+	for i := 0; i < len(parameters); i++ {
+		result = append(result, math.Sin(math.Pi*float64(parameters[i])/180))
+	}
+	return result
+}
+
+func Cos(args ...int) []float64 {
+	var result []float64
+	parameters := extract_parameters(args)
+
+	// check the args and return the appropriate function result
+	for i := 0; i < len(parameters); i++ {
+		result = append(result, math.Cos(math.Pi*float64(parameters[i])/180))
+	}
+	return result
+}
+
+func Tan(args ...int) []float64 {
+	var result []float64
+	parameters := extract_parameters(args)
+
+	// check the args and return the appropriate function result
+	for i := 0; i < len(parameters); i++ {
+		result = append(result, math.Tan(math.Pi*float64(parameters[i])/180))
+	}
+	return result
+}
