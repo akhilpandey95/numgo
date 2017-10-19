13473d94af9780db4565ddd9e8e005af631566f5
AkhilHector <akhilhector.1@gmail.com>
2017-10-19 18:42:14 -0500

    Added the following methods in ufunc/strings
    - ufunc.Strcount() for counting a substring or character over an
    nd-array
    
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- ufunc/strings.go
+++ ufunc/strings.go
@@ -5,3 +5,28 @@
  */
 
 package numgo
+
+import (
+	"strings"
+)
+
+func extract_strings(args []string) []string {
+	var strings []string
+
+	// store the parameters in a slice
+	for _, s := range args {
+		strings = append(strings, s)
+	}
+	return strings
+}
+
+func Strcount(array_one []string, args ...string) []int {
+	var result []int
+	str_args := extract_strings(args)
+
+	// check the character count and return the appropriate result
+	for i := 0; i < len(array_one); i++ {
+		result = append(result, strings.Count(array_one[i], str_args[0]))
+	}
+	return result
+}
