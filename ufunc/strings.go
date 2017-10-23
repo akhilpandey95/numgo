305d3e2ec8af6c8ba9500853eaecd7f1d3fd35a2
AkhilHector <akhilhector.1@gmail.com>
2017-10-23 00:21:58 -0500

    Added the following methods to ufunc/strings
    - ufunc.Strjoin() for joining to arrays basing on the delimiter
    - ufunc.Strtoupper() for converting every element of the array to
    uppercase
    - ufunc.Strtolower() for converting every element of the array to
    lowercase
    
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- ufunc/strings.go
+++ ufunc/strings.go
@@ -7,6 +7,7 @@
 package numgo
 
 import (
+	"log"
 	"strings"
 )
 
@@ -27,6 +28,40 @@ func Strcount(array_one []string, args ...string) []int {
 	// check the character count and return the appropriate result
 	for i := 0; i < len(array_one); i++ {
 		result = append(result, strings.Count(array_one[i], str_args[0]))
+	}
+	return result
+}
+
+func Strjoin(delimiter string, array_one, array_two []string) []string {
+	var result []string
+
+	//check the delimiter and join the arrays appropriately
+	if len(array_one) != len(array_two) {
+		log.Fatal("[ERROR]: the shapes of the arrays must be similar to perform Strjoin operation")
+	} else {
+		for i := 0; i < len(array_one); i++ {
+			result = append(result, array_one[i]+delimiter+array_two[i])
+		}
+	}
+	return result
+}
+
+func Strtoupper(array_one []string) []string {
+	var result []string
+
+	//take the args and convert every character/word of the array to uppwercase
+	for i := 0; i < len(array_one); i++ {
+		result = append(result, strings.ToUpper(array_one[i]))
+	}
+	return result
+}
+
+func Strtolower(array_one []string) []string {
+	var result []string
+
+	//take the args and covert every character/word of the array to lowercase
+	for i := 0; i < len(array_one); i++ {
+		result = append(result, strings.ToLower(array_one[i]))
 	}
 	return result
 }
