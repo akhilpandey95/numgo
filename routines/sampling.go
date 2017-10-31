ca7c64de41bb02efdf63a0b61c8b9256b8857852
AkhilHector <akhilhector.1@gmail.com>
2017-10-30 20:18:26 -0500

    Touched the following files with boilerplate code
    - linalg/vectors.go
    - routines/random.go
    - routines/sampling.go
    - routines/distributions.go
    
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

---
+++ routines/sampling.go
@@ -0,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the MPL
+*  License. If a copy of the same was not distributed with this
+*  file, You can obtain one at
+*  https://github.com/AkhilHector/pubundsci/blob/master/LICENSE.
+ */
+
+package numgo
